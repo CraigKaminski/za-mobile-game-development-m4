171cc177e9e9b7ecae33581c36c797e0ab8de7f8
Craig Kaminski <craig.j.kaminski@gmail.com>
2018-01-07 19:20:11 -0500

    Enable physics on player, ground, and platform. Add keyboard and touchscreen controls to move player left and right and jump..

--- src/states/Game.ts
+++ src/states/Game.ts
@@ -1,11 +1,23 @@
 export class Game extends Phaser.State {
+  private readonly RUNNING_SPEED = 180;
+  private readonly JUMPING_SPEED = 550;
+  private actionButton: Phaser.Button;
+  private cursor: Phaser.CursorKeys;
   private ground: Phaser.Sprite;
+  private leftArrow: Phaser.Button;
+  private platform: Phaser.Sprite;
   private player: Phaser.Sprite;
+  private rightArrow: Phaser.Button;
 
   public init() {
     this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     this.scale.pageAlignHorizontally = true;
     this.scale.pageAlignVertically = true;
+
+    this.physics.startSystem(Phaser.Physics.ARCADE);
+    this.physics.arcade.gravity.y = 1000;
+
+    this.cursor = this.input.keyboard.createCursorKeys();
   }
 
   public preload() {
@@ -21,16 +33,88 @@ export class Game extends Phaser.State {
 
   public create() {
     this.ground = this.add.sprite(0, 500, 'ground');
+    this.physics.arcade.enableBody(this.ground);
+    this.ground.body.allowGravity = false;
+    this.ground.body.immovable = true;
 
-    const platform = this.add.sprite(0, 300, 'platform');
+    this.platform = this.add.sprite(0, 300, 'platform');
+    this.physics.arcade.enableBody(this.platform);
+    this.platform.body.allowGravity = false;
+    this.platform.body.immovable = true;
 
     this.player = this.add.sprite(100, 200, 'player', 3);
     this.player.anchor.setTo(0.5);
     this.player.animations.add('walking', [0, 1, 2, 1], 6, true);
-    this.player.play('walking');
+    this.physics.arcade.enableBody(this.player);
+
+    this.createOnscreenControls();
   }
 
   public update() {
+    this.physics.arcade.collide(this.player, this.ground);
+    this.physics.arcade.collide(this.player, this.platform);
 
+    this.player.body.velocity.x = 0;
+
+    if (this.cursor.left.isDown || this.player.data.isMovingLeft) {
+      this.player.body.velocity.x = -this.RUNNING_SPEED;
+    } else if (this.cursor.right.isDown || this.player.data.isMovingRight) {
+      this.player.body.velocity.x = this.RUNNING_SPEED;
+    }
+
+    if ((this.cursor.up.isDown || this.player.data.mustJump) && this.player.body.touching.down) {
+      this.player.body.velocity.y = -this.JUMPING_SPEED;
+      this.player.data.mustJump = false;
+    }
+  }
+
+  private createOnscreenControls() {
+    this.leftArrow = this.add.button(20, 535, 'arrowButton');
+    this.rightArrow = this.add.button(110, 535, 'arrowButton');
+    this.actionButton = this.add.button(280, 535, 'actionButton');
+
+    this.leftArrow.alpha = 0.5;
+    this.rightArrow.alpha = 0.5;
+    this.actionButton.alpha = 0.5;
+
+    this.actionButton.events.onInputDown.add(() => {
+      this.player.data.mustJump = true;
+    });
+
+    this.actionButton.events.onInputUp.add(() => {
+      this.player.data.mustJump = false;
+    });
+
+    this.leftArrow.events.onInputDown.add(() => {
+      this.player.data.isMovingLeft = true;
+    });
+
+    this.leftArrow.events.onInputUp.add(() => {
+      this.player.data.isMovingLeft = false;
+    });
+
+    this.leftArrow.events.onInputOver.add(() => {
+      this.player.data.isMovingLeft = true;
+    });
+
+    this.leftArrow.events.onInputOut.add(() => {
+      this.player.data.isMovingLeft = false;
+    });
+
+    this.rightArrow.events.onInputDown.add(() => {
+      this.player.data.isMovingRight = true;
+    });
+
+    this.rightArrow.events.onInputUp.add(() => {
+      this.player.data.isMovingRight = false;
+    });
+
+    this.rightArrow.events.onInputOver.add(() => {
+      this.player.data.isMovingRight = true;
+    });
+
+    this.rightArrow.events.onInputOut.add(() => {
+      this.player.data.isMovingRight = false;
+    });
   }
 }
